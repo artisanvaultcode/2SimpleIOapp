4b5507eb0bf97892925fd2a167b48f4d5f2d6f8e
Miguel A Mayori <mayori.engineering@gmail.com>
2021-10-12 17:03:55 -0700

    -- quick fix

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -71,11 +71,9 @@ export class AuthSignInComponent implements OnInit
     signIn(): void
     {
         // Return if the form is invalid
-        if ( this.signInForm.invalid )
-        {
+        if ( this.signInForm.invalid ) {
             return;
         }
-
         // Disable the form
         this.signInForm.disable();
 
@@ -85,7 +83,7 @@ export class AuthSignInComponent implements OnInit
         // Sign in
         this._authService.signIn(this.signInForm.value)
             .then( (user: CognitoUser|any) => {
-                this._router.navigate(['client/recipients']);
+                this._router.navigate(['client/dashboard']);
             }).catch((error) => {
                 switch (error.code) {
                     case 'UserNotConfirmedException':
