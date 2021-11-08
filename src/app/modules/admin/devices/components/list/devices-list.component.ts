5a3d156fc60ad941055fa724670868c0f967a49d
Miguel A Mayori <mayori.engineering@gmail.com>
2021-11-07 19:54:12 -0800

    -- update

--- src/app/modules/admin/devices/components/list/devices-list.component.ts
+++ src/app/modules/admin/devices/components/list/devices-list.component.ts
@@ -210,6 +210,7 @@ export class DevicesListComponent implements OnInit, OnDestroy {
         return Promise.resolve();
     }
 
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     private S4(): string {
         // eslint-disable-next-line no-bitwise
         return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
